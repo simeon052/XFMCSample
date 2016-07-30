8e6ba2979f03ade96a7a42c50efebe26dc2e71e6
Satoshi Matsushita <simeon_the_city_who_fought@outlook.com>
2016-07-30 22:50:29 +0900

    Add button by codebehind

--- XFMCSample/XFMCSample/Views/FirstPage.xaml.cs
+++ XFMCSample/XFMCSample/Views/FirstPage.xaml.cs
@@ -10,9 +10,16 @@ namespace XFMCSample.Views
 {
     public partial class FirstPage : ContentPage
     {
+        private int c;
         public FirstPage()
         {
             InitializeComponent();
+            this.Button1.Clicked += Button1_ClickedEvent;
+        }
+
+        private void Button1_ClickedEvent(object sender, EventArgs e)
+        {
+            this.CountLabel.Text = $"Button is clicked [{++c}]!!!!";
         }
     }
 }
