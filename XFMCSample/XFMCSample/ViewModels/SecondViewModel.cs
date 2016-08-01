67d2fc1370a8397253896bc61727a56041023ecb
Satoshi Matsushita <simeon_the_city_who_fought@outlook.com>
2016-08-02 07:36:34 +0900

    Add ListView

--- XFMCSample/XFMCSample/ViewModels/SecondViewModel.cs
+++ XFMCSample/XFMCSample/ViewModels/SecondViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using MvvmCross.Core.ViewModels;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 
 namespace XFMCSample.ViewModels
@@ -11,6 +13,27 @@ namespace XFMCSample.ViewModels
             {
                 return new MvxCommand(() => ShowViewModel<FirstViewModel>());
             }
+        }
+
+        public class ListItem
+        {
+            public string Name { get; set; }
+            public string Image { get; set; }
+        }
+
+        private ObservableCollection<ListItem> sampleList = new ObservableCollection<ListItem>();
+        public ObservableCollection<ListItem> SampleList
+        {
+            get { return sampleList; }
+            set { sampleList = value; }
+        }
+
+        public SecondViewModel()
+        {
+            sampleList.Add(new ListItem() { Name = "Person1", Image = "Image1.jpg" });
+            sampleList.Add(new ListItem() { Name = "Person2", Image = "Image2.jpg" });
+            sampleList.Add(new ListItem() { Name = "Person3", Image = "Image2.jpg" });
+            sampleList.Add(new ListItem() { Name = "Person4", Image = "Image3.jpg" });
         }
     }
 }
