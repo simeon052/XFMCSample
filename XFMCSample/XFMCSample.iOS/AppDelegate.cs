cdeb32034b0ab2e54a1efc5cc77838caa390fd41
simeon052 <simeon052@gmail.com>
2016-08-06 16:14:38 +0900

    iOS MVVM setup

--- XFMCSample/XFMCSample.iOS/AppDelegate.cs
+++ XFMCSample/XFMCSample.iOS/AppDelegate.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.iOS.Platform;
+using MvvmCross.iOS.Views.Presenters;
+using MvvmCross.Platform;
 using UIKit;
 
 namespace XFMCSample.iOS
@@ -11,21 +15,25 @@ namespace XFMCSample.iOS
     // User Interface of the application, as well as listening (and optionally responding) to 
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : MvxApplicationDelegate
     {
-        //
-        // This method is invoked when the application has loaded and is ready to run. In this 
-        // method you should instantiate the window, load the UI into it and then make the window
-        // visible.
-        //
-        // You have 17 seconds to return from this method, or iOS will terminate your application.
-        //
+		public override UIWindow Window { get; set; }
+
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
+			Window = new UIWindow(UIScreen.MainScreen.Bounds);
 
-            return base.FinishedLaunching(app, options);
+			var presenter = new MvxIosViewPresenter(this, Window);
+
+			var setup = new Setup(this, presenter);
+			setup.Initialize();
+
+			var startup = Mvx.Resolve<IMvxAppStart>();
+			startup.Start();
+
+			Window.MakeKeyAndVisible();
+
+			return true;
         }
     }
 }
